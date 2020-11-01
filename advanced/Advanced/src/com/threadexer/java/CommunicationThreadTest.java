43b64d4f813f4d5e76d6dda9561ce637c0e25b9a
chihokyo <chihoukyo@gmail.com>
2020-11-02 01:35:09 +0900

    【Java基础】进阶-多线程-sleep()和wait()异同

--- advanced/Advanced/src/com/threadexer/java/CommunicationThreadTest.java
+++ advanced/Advanced/src/com/threadexer/java/CommunicationThreadTest.java
@@ -14,6 +14,13 @@ package com.threadexer.java;
  *  直接写的 方式 都是省略了this
  * 3.wait()，notify()，notifyAll()三个方法是定义在java.lang.Object类中。
  * 
+ *  面试题：sleep() 和 wait()的异同？
+ * 1.相同点：一旦执行方法，都可以使得当前的线程进入阻塞状态。
+ * 2.不同点：1）两个方法声明的位置不同：Thread类中声明sleep() , Object类中声明wait()
+ *          2）调用的要求不同：sleep()可以在任何需要的场景下调用。 wait()必须使用在同步代码块或同步方法中
+ *          3）关于是否释放同步监视器：如果两个方法都使用在同步代码块或同步方法中，sleep()不会释放锁，wait()会释放锁。
+ *
+ * 
  */
 public class CommunicationThreadTest {
     public static void main(String[] args) {
