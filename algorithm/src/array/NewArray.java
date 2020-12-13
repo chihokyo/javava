a624dbe5cfc233f1e3f9a0f2ee7ab45e5e266cd9
chihokyo <chihoukyo@gmail.com>
2020-12-13 23:29:14 +0900

    【算法】数组-Array-封装一个数组-实现增加方法

--- algorithm/src/array/NewArray.java
+++ algorithm/src/array/NewArray.java
@@ -1,9 +1,7 @@
 package array;
+
 /**
- * 自己封装一个数组
- * 1 capacity 表示数组的总容量，初始化大小。
- * 2 size 表示实际容量。数组第一个没有元素的位置。
- * 3 data 成员
+ * 自己封装一个数组 1 capacity 表示数组的总容量，初始化大小。 2 size 表示实际容量。数组第一个没有元素的位置。 3 data 成员
  */
 public class NewArray {
 
@@ -47,5 +45,54 @@ public class NewArray {
      */
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    /**
+     * 所有元素后添加一个元素
+     * 
+     * @param e 元素
+     */
+    public void addLast(int e) {
+        // if (size == data.length) {
+        //     throw new IllegalArgumentException("addlast failed.Array is full");
+        // }
+
+        // data[size] = e;
+        // size ++;
+        // // 虽然也可以写成 ↓ 但是并不推荐，可读性差
+        // // data[size++] = e;
+        
+        // 写完add之后可以修改成
+        add(size, e); // size表达的就是长度也就是最后一个位置
+    }
+
+    /**
+     * 在数组最前面添加一个元素
+     * 
+     * @param e 元素
+     */
+    public void addFirst(int e) {
+        add(0, e);
+    }
+
+    /**
+     * 向指定位置传递元素
+     * 
+     * @param index
+     * @param e
+     */
+    public void add(int index, int e) {
+        if (size == data.length) {
+            throw new IllegalArgumentException("addlast failed.Array is full");
+        }
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("addlast failed.Require index < 0 and index > size ");
+        }
+        for (int i = size - 1; i < index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = e;
+        size++;
+
     }
 }
