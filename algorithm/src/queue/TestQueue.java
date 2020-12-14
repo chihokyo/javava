ec865aa67efc3b69fc5ec03983fa617393679dae
chihokyo <chihoukyo@gmail.com>
2020-12-15 00:17:32 +0900

    【算法】队列-Queue-循环队列PK数组队列的速度测试

--- algorithm/src/queue/TestQueue.java
+++ algorithm/src/queue/TestQueue.java
@@ -1,5 +1,7 @@
 package queue;
 
+import java.util.Random;
+
 import org.junit.Test;
 
 /**
@@ -8,6 +10,43 @@ import org.junit.Test;
 public class TestQueue {
     public static void main(String[] args) {
 
+        int opCount = 100000; // 十万级
+
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        double time1 = testSpeedQueue(arrayQueue, opCount);
+        System.out.println("ArrayQueue, time: " + time1 + "s");
+
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        double time2 = testSpeedQueue(loopQueue, opCount);
+        System.out.println("LoopQueue, time: " + time2 + " s");
+
+        // 数组队列完败，主要是dequeue这个级别就是0(n^2)
+        // ArrayQueue, time: 49.963594867 s
+        // LoopQueue, time: 0.028540527 s
+    }
+
+    /**
+     * 封装一个统一测试时间工具
+     * 
+     * @param q       传入Integer类型的队列（多态性 既可以是循环还可以是数组）
+     * @param opCount 操作数量
+     * @return
+     */
+    private static double testSpeedQueue(Queue<Integer> q, int opCount) {
+
+        long startTime = System.nanoTime();
+
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            q.enqueue(random.nextInt(Integer.MAX_VALUE));
+        }
+        for (int i = 0; i < opCount; i++) {
+            q.dequeue();
+        }
+
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000_000_000.0;
     }
 
     // 测试一个数组队列
@@ -39,4 +78,5 @@ public class TestQueue {
             }
         }
     }
+
 }
